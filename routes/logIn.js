5335557dd3115e50409ad8b82dd2ac0bab3b0e82
ofeck <ofeckm3@gmail.com>
2020-01-22 15:19:27 +0200

    last commit for login/signup

--- routes/logIn.js
+++ routes/logIn.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const {logIn} = require('../db/api/logIn');
+const crypto = require('crypto');
 
 router.post('/', async (req, res, next) => {
     try{
         const {email , password} = req.body;
-        const user = await logIn(email, password);
-        console.log('login', user);
+        new1 = crypto.pbkdf2Sync(password, 'realtorrocks', 100000, 64, 'sha512');
+        new2 = new1.toString('base64');
+        const user = await logIn(email, new2);
         res.cookie('user',JSON.stringify(user), {maxAge: 1000*60*60*24*24});
         res.status(200).json(user);
     }catch(error){
