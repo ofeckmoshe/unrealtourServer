cc0ae53b3471f12bdc175a4de8adda0636a296b2
ofeck <ofeckm3@gmail.com>
2020-01-29 16:45:47 +0200

    Update logIn.js

--- routes/logIn.js
+++ routes/logIn.js
@@ -9,7 +9,7 @@ router.post('/', async (req, res, next) => {
         new1 = crypto.pbkdf2Sync(password, 'realtorrocks', 100000, 64, 'sha512');
         new2 = new1.toString('base64');
         const user = await logIn(email, new2);
-        res.cookie('user',JSON.stringify(user), {maxAge: 1000*60*60*24*24});
+        res.cookie('user',JSON.stringify(user), {maxAge: 1000*60*60*24});
         res.status(200).json(user);
     }catch(error){
         res.status(401).json({status: 401,errors:'Invalid email or password'});
